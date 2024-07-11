e7d70abf2464597ce6b9e09a38ab6cb1158dcd45
omarse7a <omar.seha90@gmail.com>
2024-07-11 17:14:19 +0300

    added the product details template

--- core/views.py
+++ core/views.py
@@ -9,11 +9,12 @@ def index(request):
 
 # Product related views
 def product_list(request, cat_name):
-    products = Product.objects.all()    # get all products from db
     categories = Category.objects.all() # get all categories from db
 
     # Filter by selected category
-    if cat_name != "all":
+    if cat_name == "all":
+        products = Product.objects.all()  # Get all products from db
+    else:
         category = Category.objects.get(cat_name=cat_name)
         products = category.get_cat_products()  # get all products by this category
     
@@ -22,20 +23,25 @@ def product_list(request, cat_name):
                 "categories": categories,
                 "selected_cat": cat_name, 
                }
-    return render(request, "product_list.html", context)
+    return render(request, "products/product_list.html", context)
 
 def product_search(request):
-    products = Product.objects.all()    # get all products from db
-    categories = Category.objects.all() # get all categories from db
+    categories = Category.objects.all()  # Get all categories from db
 
     # Search functionality
-    search_value = request.GET.get("search-inp")
+    search_value = request.GET.get("search-inp", "")
     if search_value:
-        products = products.filter( Q(name__icontains=search_value) | Q(category__cat_name__startswith=search_value))
+        products = Product.objects.filter(
+            Q(name__icontains=search_value) | Q(category__cat_name__startswith=search_value)
+        )
+        selected_cat = f"results for '{search_value}'"
+    else:
+        products = Product.objects.all()  # Get all products from db
+        selected_cat = "all"
     
     context = {
                 "products": products, 
                 "categories": categories,
-                "selected_cat": f"results for '{search_value}'", 
+                "selected_cat": selected_cat, 
                }
-    return render(request, "product_list.html", context)
+    return render(request, "products/product_list.html", context)
